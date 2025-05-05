6da09973d7d70bb854934002815cd88685ab0980
Mekdelawit Belay <meqdellawittesfu@gmail.com>
2025-05-05 03:03:17 +0300

    changes

--- FrontEnd/Head_Foot/script.js
+++ FrontEnd/Head_Foot/script.js
@@ -1,8 +1,8 @@
 // Update the fetch paths to:
-fetch('../../Head_Foot/header.html')
+fetch('http://localhost/website/project/et_passport_service_backend/FrontEnd/Head_Foot/header.html')
   .then(response => response.text())
   .then(data => document.getElementById('header-placeholder').innerHTML = data);
 
-fetch('../../Head_Foot/footer.html')
+fetch('http://localhost/website/project/et_passport_service_backend/FrontEnd/Head_Foot/footer.html')
   .then(response => response.text())
   .then(data => document.getElementById('footer-placeholder').innerHTML = data);
