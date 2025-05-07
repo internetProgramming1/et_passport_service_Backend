38d1bd11f732b5b0079a3df4d8e604695086be92
Meskerem Fekede <yeabsrafekede@gmail.com>
2025-05-08 01:38:25 +0300

    Resolved merge conflicts and deleted duplicates

--- FrontEnd/Head_Foot/script.js
+++
@@ -1,8 +0,0 @@
-// Update the fetch paths to:
-fetch('../../Head_Foot/header.html')
-  .then(response => response.text())
-  .then(data => document.getElementById('header-placeholder').innerHTML = data);
-
-fetch('../../Head_Foot/footer.html')
-  .then(response => response.text())
-  .then(data => document.getElementById('footer-placeholder').innerHTML = data);
