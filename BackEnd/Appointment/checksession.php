860d8426211c61520770c65c4a9e4ec2031b6936
Liben Gebremedhin <149259472+libengebremedhin@users.noreply.github.com>
2025-05-08 21:12:49 +0300

    some changes

--- BackEnd/Appointment/checksession.php
+++ BackEnd/Appointment/checksession.php
@@ -2,12 +2,6 @@
 require 'formHandler.php'; // Include your form handling code
 
 
-header("Content-Type: application/json");
-
-// Allow access from frontend
-header("Access-Control-Allow-Origin: http://127.0.0.1:5500");
-header("Access-Control-Allow-Credentials: true");
-
 echo json_encode([
     'success' => true,
     'data' => $_SESSION
